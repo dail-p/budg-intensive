940e32194b8ad017f1d2fb51ab8cb17d879a921c
dail-p <85954662+dail-p@users.noreply.github.com>
2021-08-09 22:07:01 +0300

    Сorrected 1 day tasks

--- day_1/flow_control/task_3/implementation.py
+++ day_1/flow_control/task_3/implementation.py
@@ -6,30 +6,33 @@ def get_days_count_by_month(month):
 
     Returns: количество дней
     """
+    days = 0
     if month == 'январь':
-        return 31
+        days = 31
     elif month == 'февраль':
-        return 28
+        days = 28
     elif month == 'март':
-        return 31
+        days = 31
     elif month == 'апрель':
-        return 30
+        days = 30
     elif month == 'май':
-        return 31
+        days = 31
     elif month == 'июнь':
-        return 30
+        days = 30
     elif month == 'июль':
-        return 31
+        days = 31
     elif month == 'август':
-        return 31
+        days = 31
     elif month == 'сентябрь':
-        return 30
+        days = 30
     elif month == 'октябрь':
-        return 31
+        days = 31
     elif month == 'ноябрь':
-        return 30
+        days = 30
     elif month == 'декабрь':
-        return 31
+        days = 31
     else:
-        return 0
+        days = 0
+
+    return days
 
--- day_1/flow_control/task_4/implementation.py
+++ day_1/flow_control/task_4/implementation.py
@@ -10,25 +10,27 @@ def get_next_date(some_date):
     Returns: следующая дата
     """
     kwargs = {'year': some_date.year, 'month': some_date.month, 'day': some_date.day}
-    months = [[1, 3, 5, 7, 8, 10, 12],[4, 6, 9, 11], 2]
+    long_months = (1, 3, 5, 7, 8, 10, 12)
+    short_months = (4, 6, 9, 11)
+    february = 2
 
-    if some_date.month in months[0]:
+    if some_date.month in long_months:
         if some_date.day == 31:
-            if some_date.month == months[0][6]:
+            if some_date.month == long_months[6]:
                 kwargs['year'] += 1
             kwargs['month'] = 1
             kwargs['day'] = 1
         else:
             kwargs['day'] += 1
 
-    elif some_date.month in months[1]:
+    elif some_date.month in short_months:
         if some_date.day == 30:
             kwargs['month'] += 1
             kwargs['day'] = 1
         else:
             kwargs['day'] += 1
 
-    elif some_date.month == months[2]:
+    elif some_date.month == february:
         if some_date.day == 29:
             kwargs['month'] += 1
             kwargs['day'] = 1
