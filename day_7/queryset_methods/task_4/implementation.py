2b11ba84259b4cfe403737330f4e881f79ca63b0
dail-p <85954662+dail-p@users.noreply.github.com>
2021-08-18 00:46:33 +0300

    Solved 4 task

--- day_7/queryset_methods/task_4/implementation.py
+++ day_7/queryset_methods/task_4/implementation.py
@@ -1,3 +1,9 @@
+from day_7.queryset_methods.models import (
+    OrderItem,
+    Product
+)
+
+
 def get_top_product_by_total_count_in_period(begin, end):
     """Возвращает товар, купленный в наибольшем объеме за определенный промежуток времени
 
@@ -7,4 +13,22 @@ def get_top_product_by_total_count_in_period(begin, end):
 
     Returns: возвращает наименование товара и объем
     """
-    raise NotImplementedError
+    products = Product.objects.all()
+
+    result = []
+    for product in products:
+        orderitem = OrderItem.objects.filter(product=product, order__date_formation__gte=begin,
+                                             order__date_formation__lt=end)
+        total_count = 0
+        for item in orderitem:
+            total_count += item.count
+        if total_count == 0:
+            continue
+        else:
+            result.append((product.name, int(total_count)))
+
+    result = sorted(result, key=lambda x: x[0][1], reverse=True)
+    if result:
+        return list(filter(lambda x: x[1] == result[0][1], result))
+    else:
+        return result
